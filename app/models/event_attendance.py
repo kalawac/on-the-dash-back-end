f1d8e3aa78ee55e6bc80361a8aea48ed92985aba
Katherine Abrikian <kabrikian@gmail.com>
2023-02-13 04:55:38 -0500

    reset db

--- app/models/event_attendance.py
+++ app/models/event_attendance.py
@@ -1,34 +1,34 @@
-from app import db
-from sqlalchemy.dialects.postgresql import UUID
+# from app import db
+# from sqlalchemy.dialects.postgresql import UUID
 
-# Association table for 0..* : 0..* relationship between Contact and Event
+# # Association table for 0..* : 0..* relationship between Contact and Event
 
-class EventAttendance(db.Model):
-    event_id = db.Column(UUID(as_uuid = True), db.ForeignKey('event.id'), primary_key=True)
-    participant_id = db.Column(UUID(as_uuid = True), db.ForeignKey('contact.id'), primary_key=True)
-    attended = db.Column(db.Boolean, default=False)
-    completed = db.Column(db.Boolean, default=False)
-
-
-    @classmethod
-    def new_from_dict(cls, data_dict):
-        return cls(
-            event_id=data_dict["event_id"], 
-            participant_id=data_dict["participant_id"]
-            )
+# class EventAttendance(db.Model):
+#     event_id = db.Column(UUID(as_uuid = True), db.ForeignKey('event.id'), primary_key=True)
+#     participant_id = db.Column(UUID(as_uuid = True), db.ForeignKey('contact.id'), primary_key=True)
+#     attended = db.Column(db.Boolean, default=False)
+#     completed = db.Column(db.Boolean, default=False)
 
 
-    def to_dict(self):
-        event_attendance_dict = dict()
+#     @classmethod
+#     def new_from_dict(cls, data_dict):
+#         return cls(
+#             event_id=data_dict["event_id"], 
+#             participant_id=data_dict["participant_id"]
+#             )
 
-        event_attendance_dict[self.participant_id] = dict(
-            attendance=self.attended,
-            completion=self.completed
-            )
-        return event_attendance_dict
 
-    def to_participant_dict(self):
-        return dict(
-            attendance=self.attended,
-            completion=self.completed
-            )
+#     def to_dict(self):
+#         event_attendance_dict = dict()
+
+#         event_attendance_dict[self.participant_id] = dict(
+#             attendance=self.attended,
+#             completion=self.completed
+#             )
+#         return event_attendance_dict
+
+#     def to_participant_dict(self):
+#         return dict(
+#             attendance=self.attended,
+#             completion=self.completed
+#             )
